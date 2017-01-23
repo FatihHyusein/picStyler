981c779c7418324ec81bc65740d589d0c5b97583
f14ka <f.hyuseinov@gmail.com>
2017-01-23 23:55:16 +0200

    heroku url

--- app/config/index.js
+++ app/config/index.js
@@ -1,5 +1,5 @@
 let config = {
-  API_BASE_URL: process.env.API_BASE_URL || 'https://picstyler.herokuapp.com/',
+  API_BASE_URL: process.env.API_BASE_URL || '',
   PHP_API_BASE_URL: process.env.PHP_API_BASE_URL || 'http://87.120.75.34/picStyler/src/public'
 };
 
